a1ad42ecb66110732a4776280ccf02a39f9e2d41
Bum Kim <bumcrystlbum@gmail.com>
2024-09-14 13:28:36 -0400

    functional barebones quicksort

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,105 @@
-import Image from "next/image";
+'use client'
+
+import React from 'react'
+
+let swaps: number[][] = []
 
 export default function Home() {
+  // generate random numbers
+  const initialValues = Array.from(generateRandomArray(8, 12))
+  const [values, setValues] = React.useState([...initialValues])
+  function handleSort() {
+    // fill swaps
+    let newValues = [...values]
+    quicksort(values, 0, values.length - 1)
+    for (let i = 0; i < swaps.length; i++) {
+      setTimeout(
+        () => {
+          ;[newValues[swaps[i][0]], newValues[swaps[i][1]]] = [
+            newValues[swaps[i][1]],
+            newValues[swaps[i][0]],
+          ]
+          setValues([...newValues])
+        },
+        500 * (i + 1)
+      )
+    }
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex flex-col w-fill h-fill">
+      <div className="flex gap-1 justify-center w-full">
+        {values.map((v, id) => (
+          <div
+            className="bg-black w-[100px] h-[100px]"
+            key={id}
+            id={`box-${id}`}
+            style={{ height: `${v * 50}px` }}
+          ></div>
+        ))}
       </div>
+      <button
+        className="border-2 w-fit mx-auto my-2 p-2 rounded hover:bg-green-50"
+        onClick={(e) => {
+          e.preventDefault()
+          handleSort()
+        }}
+      >
+        Sort
+      </button>
+    </div>
+  )
+}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function generateRandomArray(quantity: number, max: number) {
+  const set = new Set<number>()
+  while (set.size < quantity) {
+    set.add(Math.floor(Math.random() * max) + 1)
+  }
+  return set
+}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+function getRandomInt(min: number, max: number) {
+  const minCeiled = Math.ceil(min)
+  const maxFloored = Math.floor(max)
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled) // The maximum is exclusive and the minimum is inclusive
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+function partition(arr: number[], low: number, high: number) {
+  const pivot = arr[high]
+  let j = low
+  for (let i = low; i < high; i++) {
+    if (arr[i] <= pivot) {
+      ;[arr[i], arr[j]] = [arr[j], arr[i]]
+      if (i !== j) {
+        swaps.push([i, j])
+      }
+      j++
+    }
+  }
+  // SHOW this swap
+  ;[arr[j], arr[high]] = [arr[high], arr[j]]
+  if (j !== high) {
+    swaps.push([j, high])
+  }
+  return j
+}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+function partition_r(arr: number[], low: number, high: number) {
+  // SHOW original pivot location
+  const pivotIndex = getRandomInt(low, high + 1)
+  ;[[arr[high], arr[pivotIndex]]] = [[arr[pivotIndex], arr[high]]]
+  if (pivotIndex !== high) {
+    swaps.push([pivotIndex, high])
+  }
+  return partition(arr, low, high)
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+function quicksort(nums: number[], low: number, high: number) {
+  if (low < high) {
+    // SHOW which pivot is chosen
+    const pivotIndex = partition_r(nums, low, high)
+    // recurse into left and right side of pivot
+    quicksort(nums, low, pivotIndex - 1)
+    quicksort(nums, pivotIndex + 1, high)
+  }
 }
